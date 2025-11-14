0f0cadf756ac954476ce9eb8709d345f4344585e
Muhamad Rosif Al Khikam <110785229+MuhamadRosif@users.noreply.github.com>
2025-11-14 19:21:08 +0700

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Quiz App — Setup Awal</h1>
+      <p>Modul berikutnya: Landing, QuizRound, Leaderboard, AdminPanel</p>
+    </div>
+  )
+}
